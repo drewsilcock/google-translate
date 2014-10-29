6f3aad0866d083ed9e00d17a794bb3f8587d3c54
drewsberry <drew@drewsilcock.co.uk>
2014-10-29 01:47:31 +0000

    Add simple setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="google_translate",
+      version="1.0",
+      description="Google Translate Python Interface",
+      author="Drew Silcock",
+      author_email="drew@drewsilcock.co.uk",
+      packages=["google_translate"],
+      )
